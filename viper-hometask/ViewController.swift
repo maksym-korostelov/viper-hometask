2ad980c0219eede2f1fa5bed36715f89756f33d1
Maksym Korostelov <maksym_korostelov@epam.com>
2020-02-21 10:19:20 +0200

    Initial Commit

---
+++ viper-hometask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viper-hometask
+//
+//  Created by Maksym Korostelov on 2/21/20.
+//  Copyright © 2020 Maksym Korostelov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
